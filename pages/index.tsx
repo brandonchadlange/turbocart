72f36deb263eaeb697673f4e34e758c94c243b34
Brandon <brandon.lange@aboutyou.com>
2023-03-25 18:57:55 +0100

    trying things!

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,55 +24,53 @@ const hammerSmithOne = Hammersmith_One({
 
 export default function Home() {
   return (
-    <>
-      <AppShell padding={0}>
-        <BackgroundImage src="banner.jpg" h="100vh">
-          <Overlay>
-            <Stack align="stretch" justify="space-around" h="100%" px={60}>
-              <Flex justify="space-between">
-                <Flex gap="sm" w={1000} align="center">
-                  <Image alt="logo" src={WhiteLogo} height={30} width={30} />
-                  <Title
-                    className={hammerSmithOne.className}
-                    color="white"
-                    size={30}
-                  >
-                    Brain Fuel
-                  </Title>
-                </Flex>
-                <Flex gap="md">
-                  <Button color="yellow" variant="outline" radius="xl">
-                    View order
-                  </Button>
-                </Flex>
-              </Flex>
-              <Card w={1000} bg="transparent" p={0}>
-                <Title color="white" size={80} style={{ lineHeight: "62px" }}>
-                  Kids Lunch? <br />
-                  <Text color="yellow">Sorted!</Text>
+    <AppShell padding={0}>
+      <BackgroundImage src="banner.jpg" h="100vh">
+        <Overlay>
+          <Stack align="stretch" justify="space-around" h="100%" px={60}>
+            <Flex justify="space-between">
+              <Flex gap="sm" w={1000} align="center">
+                <Image alt="logo" src={WhiteLogo} height={30} width={30} />
+                <Title
+                  className={hammerSmithOne.className}
+                  color="white"
+                  size={30}
+                >
+                  Brain Fuel
                 </Title>
-                <Text size={40} color="white" mt="lg" maw={500}>
-                  Order your childrens meals up to two weeks in advance!
-                </Text>
-                <Flex gap="md" mt={40}>
-                  <Button
-                    component={Link}
-                    href="/students"
-                    w={180}
-                    radius="xl"
-                    color="yellow"
-                    style={{ color: "black" }}
-                    size="lg"
-                  >
-                    Place order
-                  </Button>
-                </Flex>
-              </Card>
-            </Stack>
-          </Overlay>
-        </BackgroundImage>
-      </AppShell>
-    </>
+              </Flex>
+              <Flex gap="md">
+                <Button color="yellow" variant="outline" radius="xl">
+                  View order
+                </Button>
+              </Flex>
+            </Flex>
+            <Card w={1000} bg="transparent" p={0}>
+              <Title color="white" size={80} style={{ lineHeight: "62px" }}>
+                Kids Lunch? <br />
+                <Text color="yellow">Sorted!</Text>
+              </Title>
+              <Text size={40} color="white" mt="lg" maw={500}>
+                Order your childrens meals up to two weeks in advance!
+              </Text>
+              <Flex gap="md" mt={40}>
+                <Button
+                  component={Link}
+                  href="/students"
+                  w={180}
+                  radius="xl"
+                  color="yellow"
+                  style={{ color: "black" }}
+                  size="lg"
+                >
+                  Place order
+                </Button>
+              </Flex>
+            </Card>
+          </Stack>
+        </Overlay>
+      </BackgroundImage>
+    </AppShell>
   );
 }
 
