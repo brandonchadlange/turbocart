4f2a9dc2c9dcf40127e4627b3696757ec728da8c
Brandon <brandon.lange@aboutyou.com>
2023-03-31 16:15:44 +0200

    updated grades

--- pages/api/grade/index.ts
+++ pages/api/grade/index.ts
@@ -2,6 +2,27 @@ import { RouteHandler } from "@/backend/utility/route-handler";
 
 export default RouteHandler({
   GET(req, res) {
-    res.status(200).send(["1A", "1B", "2A", "2B", "8 - 12 (Pickup)"]);
+    res
+      .status(200)
+      .send([
+        "Little Mount",
+        "RS",
+        "RM",
+        "1E",
+        "1C",
+        "2D",
+        "2DT",
+        "3W",
+        "3P",
+        "4R",
+        "4N",
+        "5H",
+        "5L",
+        "6M",
+        "6S",
+        "7S",
+        "7V",
+        "8 - 12 (pickup)",
+      ]);
   },
 });
