a8c17ce41e74be1576ce835afd565543f41e80fb
Brandon <brandon.lange@aboutyou.com>
2026-01-07 14:56:15 +0100

    update grades

--- pages/api/grade/index.ts
+++ pages/api/grade/index.ts
@@ -1,43 +1,24 @@
 import { RouteHandler } from "@/backend/utility/route-handler";
 
-// OLD GRADES
-// "Little Mount",
-// "RR R",
-// "RR M",
-// "RR P",
-// "RN",
-// "1S",
-// "1F",
-// "2D",
-// "2DT",
-// "3W",
-// "3P",
-// "4L",
-// "4R",
-// "5H",
-// "5M",
-// "6K",
-// "6L",
-// "7S",
-// "7SK",
-// "8 - 12 (pickup)",
-
 export default RouteHandler({
   GET(req, res) {
     res
       .status(200)
       .send([
-        "Collect Picnic",
-        "Little Mount",
-        "RR RR",
-        "R RV",
-        "R RG",
-        "1S",
-        "2D",
+        "RM",
+        "1P",
+        "1D",
         "2DT",
-        "3P",
-        "3W",
-        "4 - 7 (pickup)",
+        "3B",
+        "3G",
+        "4G",
+        "4L",
+        "5A",
+        "5V",
+        "6C",
+        "6J",
+        "7A",
+        "7C",
         "8 - 12 (pickup)",
       ]);
   },
